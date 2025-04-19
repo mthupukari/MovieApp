0e887219147d1c2700a491ba3931c48bf7289f61
Mahit Thupukari <mahit@umd.edu>
2025-04-19 00:36:54 -0400

    home page

--- services/api.ts
+++ services/api.ts
@@ -1,20 +1,24 @@
-export const TMBD_CONFIG = {
-    BASE_URL: 'https://api.themoviedb.org/3', 
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
-    }
-}
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    },
+};
 
-export const fetchMovies = async ({ query }: {query: string}) => {
-    const endpoint = query 
-        ? '${TMBD_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}'
-        : '${TMBD_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc';
+export const fetchMovies = async ({
+    query,
+}: {
+    query: string;
+ }) => {
+    const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endpoint, {
         method: 'GET',
-        headers: TMBD_CONFIG.headers,
+        headers: TMDB_CONFIG.headers,
     });
 
     if (!response.ok) {
@@ -26,16 +30,3 @@ export const fetchMovies = async ({ query }: {query: string}) => {
     return data.results;
 }
 
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2JhN2E0YTdhMDdhNzZmMDJlNzI3NjZhMzIyY2FhNyIsIm5iZiI6MTc0NTAxODUzMy4zNDIwMDAyLCJzdWIiOiI2ODAyZGVhNWIxMTNmZjg3MjNkOWI3N2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.17pkdMxSL2UKOq4OMwipLAm7Of5ZcTnOlimCRHRvKbM'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
